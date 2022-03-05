54d929e99b73b7c1eebc2f05f1ba66602dcd8368
lucio <syy2585@naver.com>
2022-03-06 05:15:22 +0900

    SFP-33 Modify: get_co_occurrence_words function

--- AiTextModule/associated_word.py
+++ AiTextModule/associated_word.py
@@ -2,17 +2,27 @@ import numpy as np
 import pandas as pd
 
 from collections import defaultdict
+from collections import Counter
+
+from itertools import islice
+
+from keyword_preprocess import get_preprocessed_keyword
 
 import warnings
 warnings.filterwarnings('ignore')
 
 '''
     - 단일 문서에서 출현한 명사 리스트에 대하여 검색된 키워드를 바탕으로 연관 검색어를 도출하는 클래스
+    
     - param: noun_list (기사 본문 하나의 문서에서 추출된 중복이 적용된 명사 리스트 => mecab을 이용한 형태소 분석으로 도출됨)
+             bm_25_score (elastic-search index에서 받아온 키워드 점수)
+
+    - elastic-search index로부터 해당 키워드에 대하여 점수가 높은 문서의 명사 리스트와 bm_25 점수를 받음
 '''
 class AssociatedWords():
-    def __init__(self, noun_list) -> None:
+    def __init__(self, noun_list:list, bm_25_score:float) -> None:
         self.noun_list = noun_list
+        self.bm_25_score = bm_25_score
 
 
     '''
@@ -22,7 +32,7 @@ class AssociatedWords():
 
         - return: co_occurrence_df
     '''
-    def get_co_occurrence(self, cnt=1, window_size=3) -> pd.DataFrame:
+    def get_co_occurrence_matrix(self, cnt=1, window_size=3) -> pd.DataFrame:
         word_dic = defaultdict(int)
         vocab = set()
 
@@ -59,29 +69,83 @@ class AssociatedWords():
     '''
         - 특정 검색어에 대하여 동반 출현 빈도 점수 높은 연관 단어 도출 (단일 문서 기준)
 
-        - param: keyword (사용자가 검색한 단어 => keyword_preprocess.py에서 정제)
+        - param: keyword_list (사용자가 검색한 문장 또는 단어에 대하여 뽑아낸 명사 리스트 => keyword_preprocess.py에서 정제)
                  top_size (연관 단어 개수, default: 5)
 
-        - return: co_occurrence_words (해당 키워드에 대한 문서 내 연관 단어 리스트)
+        - return: co_occurrence_words (해당 키워드에 대한 문서 내 (연관 단어, 점수) 리스트)
     '''
-    def get_co_occurrence_words(self, keyword:str, top_size=5) -> list:
+    def get_co_occurrence_words(self, keyword_list:list, top_size=5) -> list:
 
-        # 해당 키워드에 대한 문서 내 연관 단어 리스트
-        co_occurrence_words = []
+        related_keywords_list = []
+        # 한 키워드에 대한 연관검색어 리스트 생성
+        for keyword in keyword_list:
+            try:
+                correlated_words = self.co_occurrence_df[keyword].to_dict()
+                correlated_words = {word:score for word, score in correlated_words.items() if score != 0}
+                correlated_words = dict(sorted(correlated_words.items(), key=lambda x: x[1], reverse=True))
 
-        try:
-            correlated_words = self.co_occurrence_df[keyword].to_dict()
-            correlated_words = {word:score for word, score in correlated_words.items() if score != 0}
-            correlated_words = dict(sorted(correlated_words.items(), key=lambda x: x[1], reverse=True))
-            co_occurrence_words = list(correlated_words.keys())
+                # 연관어, 동반출현빈도 점수 추가
+                co_occurrence_words = list(correlated_words.items())
+                related_keywords_list.append(co_occurrence_words)
 
-            return co_occurrence_words[:top_size]
+            # 검색 키워드가 동반 출현 빈도 매트릭스 행, 열에 존재하지 않을 경우
+            except:
+                pass
 
-        # 찾고자 하는 키워드가 문서(동반 출현 빈도 matrix)에 없을 경우
-        except:
-            co_occurrence_words.append('None')
+        # 키워드 별 연관단어 2차원 리스트 1차원 변환
+        related_keywords_1d_list = sum(related_keywords_list, [])    
+        related_keywords_1d_dic = [{related_keyword[0] : related_keyword[1]} for related_keyword in related_keywords_1d_list]
+        
+        # 각 키워드의 동반출현빈도 합산
+        total_dic = {}
+        for i in related_keywords_1d_dic:
+            total_dic = Counter(total_dic) + Counter(i)
 
-            return co_occurrence_words
+        total_dic = dict(total_dic)
+
+        # 검색 키워드와 동일한 연관단어 삭제 => 검색결과 페이지의 대시보드에 있는 연관검색어의 컨셉과 부합하지 않기 때문
+        for keyword in keyword_list:
+            try:
+                del total_dic[keyword]
+            except:
+                pass
+
+        # 각 연관키워드에 대한 동반출현빈도 수 * bm_25_score => 연관 키워드 간 점수 차등성 주기 위함
+        total_dic = {text:(score * self.bm_25_score) for text, score in total_dic.items()}
+
+        # 점수 오름차순 정렬
+        co_occurrence_words = list(sorted(total_dic.items(), key=lambda x: x[1], reverse=True))[:top_size]
+        
+        return co_occurrence_words
+
+
+def get_total_co_occurrence(doc_info_list:list, search_text_list:list, top_size=5) -> dict:
+    related_words_list = []
+    # 개별 문서에 대한 연관어 및 해당 동반출현빈도 점수 도출
+    for doc_info in doc_info_list:
+        associated_word = AssociatedWords(doc_info['nouns'], doc_info['bm25'])
+        associated_word.get_co_occurrence_matrix()
+        related_words = associated_word.get_co_occurrence_words(search_text_list)
+        related_words_list.append(related_words)
+
+    # 문서 별 연관단어 2차원 리스트 1차원 변환
+    related_words_1d_list = sum(related_words_list, [])    
+    related_words_1d_dic = [{related_keyword[0] : related_keyword[1]} for related_keyword in related_words_1d_list]
+    
+    # 각 문서의 최종 연관키워드 및 동반출현빈도 최종 합산
+    # 모든 문서에 대하여 해당 키워드의 연관검색어 도출
+    total_dic = {}
+    for i in related_words_1d_dic:
+        total_dic = Counter(total_dic) + Counter(i)
+    total_dic = dict(total_dic)
+
+    # 점수 순 정렬
+    total_dic = dict(sorted(total_dic.items(), key=lambda x: x[1], reverse=True))
+
+    # 모든 문서에 대한 해당 키워드의 연관검색어 및 점수 딕셔너리 도출
+    total_co_occurrence_words = dict(islice(total_dic.items(), top_size))
+
+    return total_co_occurrence_words
 
 
 if __name__ == '__main__':
@@ -102,9 +166,62 @@ if __name__ == '__main__':
                             '무단', '전재', '배포', '금지'
                     ]
 
-    associated_word = AssociatedWords(sample_noun_list)
-    df = associated_word.get_co_occurrence()
-    # print(df)
+    '''
+        - 사용자가 검색한 단어 명사 품사 태깅
+        - 명사 품사 태깅된 해당 리스트는 향후
+         1. 자바에 다시 전달하여 자바가 이 전처리된 명사들을 보고 엘라스틱 인덱스 조회 시행
+         2. 인덱스 조회 후 해당 모듈에 정렬 조회된 nouns 필드 결과 값 전달
+         3. 받은 nouns 필드 결과 값들을 바탕으로 연관어분석 시행하여 해당 연관어의 출현빈도 점수와 함께 자바에 다시 전달
+    '''
+    # 사용자가 검색한 단어 명사 품사 태깅, 자바에 다시 전달
+    sample_search_text = '서비스를 보장하자'
+    sample_search_text_list = get_preprocessed_keyword(sample_search_text)
 
-    words = associated_word.get_co_occurrence_words('인권', top_size=7)
-    print(words)
+    # 자바에서 해당 모듈에 건네줘야 할 데이터 양식
+    sample_doc_info = [
+        {
+            '_doc':0,
+            'nouns':[ 
+                            '유엔', '인권', '전문가', '형법', '낙태', '처벌', '한국', '우려', '유엔', 
+                            '인권', '사회', '산하', '전문가', '한국', '정부', '낙태', '처벌', '관련', 
+                            '개정안', '우려', '여성', '차별', '실무', '위원회', '건강', '특별', '관', 
+                            '여성', '폭력', '특별', '관은', '현지', '시간', '낙태', '규제', '형법', 
+                            '계속', '사용', '점', '우려', '여성', '임신', '형사', '처벌', '상기', 
+                            '국제', '인권', '규범', '임신', '중절', '범죄', '필요', '조처', '합법', 
+                            '안전', '낙태', '서비스', '접근권', '보장', '조처', '촉구', '강조', '합법', 
+                            '임신', '절', '접근', '대기', '기간', '의무', '상담', '의료', '필요', '근거', 
+                            '차별', '장애물', '제거', '권고', '헌법', '재판소', '낙태죄', '헌법', '합치', 
+                            '결정', '현행', '형법', '지난해', '개정', '주문', '정부', '지난해', '임신', '이내', 
+                            '낙태', '허용', '내용', '형법', '모자', '보건', '법', '개정안', '발의', '국회', 
+                            '계류', '법률', '미비', '낙태', '형사', '처벌', '규정', '형법', '조항', '지난해', 
+                            '일부', '효력', '상실', '이보배', '한경', '닷컴', '객원', '기자', '한국', '경제', 
+                            '무단', '전재', '배포', '금지'
+                    ],
+            'bm25':2.5
+        },
+        {
+            '_doc':1,
+            'nouns':[ 
+                            '유엔', '인권', '전문가', '형법', '낙태', '처벌', '한국', '우려', '유엔', 
+                            '인권', '사회', '산하', '전문가', '한국', '정부', '낙태', '처벌', '관련', 
+                            '개정안', '우려', '여성', '차별', '실무', '위원회', '건강', '특별', '관', 
+                            '여성', '폭력', '특별', '보장', '현지', '시간', '낙태', '규제', '형법', 
+                            '계속', '사용'
+                    ],
+            'bm25':3.7
+        },
+        {
+            '_doc':2,
+            'nouns':[ 
+                            '유엔', '인권', '전문가', '형법', '낙태', '처벌', '한국', '우려', '유엔', 
+                            '인권', '사회', '산하', '전문가', '한국', '정부', '낙태', '처벌', '관련', 
+                            '개정안', '우려', '여성', '차별', '실무', '위원회', '건강', '특별', '관', 
+                            '여성', '폭력', '특별', '현지', '시간', '낙태', '규제', '형법', 
+                            '계속', '사용'
+                    ],
+            'bm25':5.0
+        }
+    ]
+
+    sample_co_words = get_total_co_occurrence(sample_doc_info, sample_search_text_list, top_size=7)
+    print(sample_co_words)
