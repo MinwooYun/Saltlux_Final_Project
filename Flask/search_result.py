193ad2f50b824cdc44a3b7bf21bd388486fec882
lucio <syy2585@naver.com>
2022-03-15 21:26:12 +0900

    SFP-39 Modify: associated_word.py and search_result.py

--- Flask/search_result.py
+++ Flask/search_result.py
@@ -3,9 +3,6 @@ from flask import Flask, request, jsonify, make_response
 from data_preprocess.keyword_preprocess import get_preprocessed_keyword
 from data_analysis.associated_word import get_total_co_occurrence
 
-import json
-import ast
-
 import sys
 [sys.path.append(i) for i in ['.', '..']]
 
@@ -13,40 +10,37 @@ app = Flask(__name__)
 
 '''
     - 검색 문자열 형태소 분석하여 품사 태깅된 명사 리스트 자바에 전달
-    - uri: /results
+    - uri: /news
 '''
-@app.route('/results', methods=['GET'])
+@app.route('/news', methods=['GET', 'POST'])
 def get_refined_keyword():
 
-    data = request.args.get('search')
-    print(data)
+    # 사용자 검색 문자열
+    search_text = request.args.get('question')
 
-    # 사용자가 검색한 키워드
-    # search_keyword = data.split('+')[0]
-
-    # 검색 결과 문서 리스트
-    # doc_info_str = data.split('+')[1]
-    # doc_info_list = json.loads(json.dumps(doc_info_str))
-    # doc_info_list = ast.literal_eval(doc_info_list)
-
-    refined_data_list = get_preprocessed_keyword(str(data))
+    # 검색 문자열 명사 리스트 변환
+    refined_data_list = get_preprocessed_keyword(search_text)
     print(refined_data_list)
 
-    sample_doc_info = [{"nouns": '유엔 인권 여성 인권 보장 필요 유엔 여성 인권',"bm25":2.5}]
+    # 검색 결과 콘텐츠 정보
+    if request.method == 'POST':
+        print(request.is_json)
+        search_result_list = request.get_json(silent=True)
+        print(search_result_list)
 
-    associated_words_dic = get_total_co_occurrence(
-                                    doc_info_list=sample_doc_info, 
-                                    search_text_list=refined_data_list,
-                                    word_top_size=5,
-                                    top_size=20
-                                    )
+        associated_words_dic = get_total_co_occurrence(
+                                            doc_info_list=search_result_list, 
+                                            search_text_list=refined_data_list,
+                                            word_top_size=5,
+                                            top_size=30
+                                        )
 
-    dic_json = jsonify(associated_words_dic)
+        associated_dic_json = jsonify(associated_words_dic)
 
-    response = make_response(dic_json)
-    response.headers.add("Access-Control-Allow-Origin", "*")
+        response = make_response(associated_dic_json)
+        response.headers.add("Access-Control-Allow-Origin", "*")
 
-    return response
+        return response
 
 
 if __name__ == '__main__':
